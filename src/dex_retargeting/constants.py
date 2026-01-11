4eb4ba38e0a20a853b5534679cf369f0ef9c7450
mattephi <ialentev@icloud.com>
2026-01-11 22:59:21 +0900

    feat: add ROHand robot hand support
    Add configuration and constants for ROHand underactuated robot hand:
    - Add rohand to RobotName enum and ROBOT_NAME_MAP
    - Add teleop configs for left and right hand (vector retargeting)
    - Uses tip links for fingertip tracking
    - Compatible with dex-urdf rohand assets

--- src/dex_retargeting/constants.py
+++ src/dex_retargeting/constants.py
@@ -29,6 +29,7 @@ class RobotName(enum.Enum):
     ability = enum.auto()
     inspire = enum.auto()
     panda = enum.auto()
+    rohand = enum.auto()
 
 
 class RetargetingType(enum.Enum):
@@ -52,6 +53,7 @@ ROBOT_NAME_MAP = {
     RobotName.ability: "ability_hand",
     RobotName.inspire: "inspire_hand",
     RobotName.panda: "panda_gripper",
+    RobotName.rohand: "rohand",
 }
 
 ROBOT_NAMES = list(ROBOT_NAME_MAP.keys())
